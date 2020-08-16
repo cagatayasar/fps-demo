7f1062a99479fabc83e6ac27aa79e2196dc4e7bf
cagatayasar <cagatayasar@hotmail.com>
2020-08-17 00:53:16 +0300

    3D world

--- fps-demo/src/tutorial6_camera/main.cpp
+++ fps-demo/src/tutorial6_camera/main.cpp
@@ -9,6 +9,7 @@
 #include "camera.h"
 
 #include <iostream>
+#include <string>
 
 void framebuffer_size_callback(GLFWwindow* window, int width, int height);
 void process_input(GLFWwindow* window);
@@ -22,8 +23,8 @@ struct WallRotation
 };
 
 // settings
-const unsigned int SCREEN_WIDTH = 800;
-const unsigned int SCREEN_HEIGHT = 600;
+const unsigned int SCREEN_WIDTH = 1600;
+const unsigned int SCREEN_HEIGHT = 900;
 
 // camera
 Camera camera(glm::vec3(0.0f, 0.0f, 3.0f));
@@ -66,78 +67,51 @@ int main()
 	glEnable(GL_DEPTH_TEST);
 
 	// shaders
-	Shader ourShader("./shaders/tutorial5_coord.vert", "./shaders/tutorial5_coord.frag");
+	std::string shaderPath = "C:/Users/cagat/projects/fps-demo/fps-demo/shaders/";
+	Shader basicShader((shaderPath + "basic.vert").c_str(), (shaderPath + "basic.frag").c_str());
 
 	float floorVertices[] = {
-		 0.0f, -0.5f,  0.0f, 0.0f, 0.0f,
-		10.0f, -0.5f,  0.0f, 1.0f, 0.0f,
-		10.0f, -0.5f, 10.0f, 1.0f, 1.0f,
-		10.0f, -0.5f, 10.0f, 1.0f, 1.0f,
-		 0.0f, -0.5f, 10.0f, 0.0f, 1.0f,
-		 0.0f, -0.5f,  0.0f, 0.0f, 0.0f,
-		/*-0.5f, -0.5f, -0.5f,  0.0f, 0.0f,
-		 0.5f, -0.5f, -0.5f,  1.0f, 0.0f,
-		 0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
-		 0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
-		-0.5f,  0.5f, -0.5f,  0.0f, 1.0f,
-		-0.5f, -0.5f, -0.5f,  0.0f, 0.0f,
-
-		-0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
-		 0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
-		 0.5f,  0.5f,  0.5f,  1.0f, 1.0f,
-		 0.5f,  0.5f,  0.5f,  1.0f, 1.0f,
-		-0.5f,  0.5f,  0.5f,  0.0f, 1.0f,
-		-0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
-
-		-0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
-		-0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
-		-0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
-		-0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
-		-0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
-		-0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
-
-		 0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
-		 0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
-		 0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
-		 0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
-		 0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
-		 0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
-
-		-0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
-		 0.5f, -0.5f, -0.5f,  1.0f, 1.0f,
-		 0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
-		 0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
-		-0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
-		-0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
-
-		-0.5f,  0.5f, -0.5f,  0.0f, 1.0f,
-		 0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
-		 0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
-		 0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
-		-0.5f,  0.5f,  0.5f,  0.0f, 0.0f,
-		-0.5f,  0.5f, -0.5f,  0.0f, 1.0f*/
+		 0.0f, 0.0f,  0.0f, 0.0f, 0.0f,
+		20.0f, 0.0f,  0.0f, 5.0f, 0.0f,
+		20.0f, 0.0f, 20.0f, 5.0f, 5.0f,
+		20.0f, 0.0f, 20.0f, 5.0f, 5.0f,
+		 0.0f, 0.0f, 20.0f, 0.0f, 5.0f,
+		 0.0f, 0.0f,  0.0f, 0.0f, 0.0f
 	};
+	glm::vec3 floorPosition = glm::vec3(-10.0f, -2.0f, -10.0f);
+
+	float wallVertices[] = {
+		 0.0f,  0.0f,  0.0f, 0.0f, 0.0f,
+		20.0f,  0.0f,  0.0f, 5.0f, 0.0f,
+		20.0f,  4.0f,  0.0f, 5.0f, 1.0f,
+		20.0f,  4.0f,  0.0f, 5.0f, 1.0f,
+		 0.0f,  4.0f,  0.0f, 0.0f, 1.0f,
+		 0.0f,  0.0f,  0.0f, 0.0f, 0.0f
+	};
+
 	glm::vec3 wallPositions[] = {
-		glm::vec3( 0.0f,  2.0f,  0.0f),
-		glm::vec3(10.0f,  2.0f,  0.0f),
-		glm::vec3(10.0f,  2.0f, 10.0f),
-		glm::vec3( 0.0f,  2.0f, 10.0f),
+		glm::vec3(-10.0f, -2.0f,  10.0f),
+		glm::vec3( 10.0f, -2.0f,  10.0f),
+		glm::vec3( 10.0f, -2.0f, -10.0f),
+		glm::vec3(-10.0f, -2.0f, -10.0f),
 	};
-	glm::vec3 floorPosition = glm::vec3(0.0f, -0.5f, 0.0f);
-
-	WallRotation wallRotations[2];
-	wallRotations[0].angle = 0.0f;
-	wallRotations[0].rotationVector = glm::vec3(0.0f, 0.0f, 0.0f);
-	wallRotations[1].angle = 0.0f;
-	wallRotations[1].rotationVector = glm::vec3(0.0f, 0.0f, 0.0f);
+	WallRotation wallRotations[4];
+	wallRotations[0].angle =   0.0f;
+	wallRotations[1].angle =  90.0f;
+	wallRotations[2].angle = 180.0f;
+	wallRotations[3].angle = 270.0f;
+	wallRotations[0].rotationVector = glm::vec3(0.0f, 1.0f, 0.0f);
+	wallRotations[1].rotationVector = glm::vec3(0.0f, 1.0f, 0.0f);
+	wallRotations[2].rotationVector = glm::vec3(0.0f, 1.0f, 0.0f);
+	wallRotations[3].rotationVector = glm::vec3(0.0f, 1.0f, 0.0f);
 
 	// initialize objects
-	unsigned int VAO, VBO;
-	glGenVertexArrays(1, &VAO);
-	glGenBuffers(1, &VBO);
-	// load data
-	glBindVertexArray(VAO);
-	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+	unsigned int VAOs[2], VBOs[2];
+	glGenVertexArrays(2, VAOs);
+	glGenBuffers(2, VBOs);
+	// floor
+	glBindVertexArray(VAOs[0]);
+	glBindBuffer(GL_ARRAY_BUFFER, VBOs[0]);
 	glBufferData(GL_ARRAY_BUFFER, sizeof(floorVertices), floorVertices, GL_STATIC_DRAW);
 	// position attribute
 	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)0);
@@ -146,11 +120,21 @@ int main()
 	glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)(3 * sizeof(float)));
 	glEnableVertexAttribArray(1);
 
+	// walls
+	glBindVertexArray(VAOs[1]);
+	glBindBuffer(GL_ARRAY_BUFFER, VBOs[1]);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(wallVertices), wallVertices, GL_STATIC_DRAW);
+	// position attribute
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)0);
+	glEnableVertexAttribArray(0);
+	// texture coord attribute
+	glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)(3 * sizeof(float)));
+	glEnableVertexAttribArray(1);
 
-	// texture1
-	unsigned int texture1, texture2;
-	glGenTextures(1, &texture1);
-	glBindTexture(GL_TEXTURE_2D, texture1);
+
+	unsigned int floorTexture, wallTexture;
+	glGenTextures(1, &floorTexture);
+	glBindTexture(GL_TEXTURE_2D, floorTexture);
 	// wrapping/filtering options
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
@@ -164,12 +148,12 @@ int main()
 		glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
 		glGenerateMipmap(GL_TEXTURE_2D);
 	} else {
-		std::cout << "Failed to load texture1" << std::endl;
+		std::cout << "Failed to load texture: floor" << std::endl;
 	}
 	stbi_image_free(data);
-	// texture 2
-	glGenTextures(1, &texture2);
-	glBindTexture(GL_TEXTURE_2D, texture2);
+
+	glGenTextures(1, &wallTexture);
+	glBindTexture(GL_TEXTURE_2D, wallTexture);
 	// wrapping/filtering options
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
@@ -180,14 +164,8 @@ int main()
 		glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
 		glGenerateMipmap(GL_TEXTURE_2D);
 	} else {
-		std::cout << "Failed to load texture2" << std::endl;
+		std::cout << "Failed to load texture: wall" << std::endl;
 	}
-
-	// tell opengl for each sampler to which texture unit it belongs to (only has to be done once)
-	ourShader.use();
-	ourShader.setInt("texture1", 0);
-	ourShader.setInt("texture2", 1);
-
 
 	// Wireframe mode:
 	// glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
@@ -205,34 +183,37 @@ int main()
 
 		// rectangle drawing
 		glActiveTexture(GL_TEXTURE0);
-		glBindTexture(GL_TEXTURE_2D, texture1);
+		glBindTexture(GL_TEXTURE_2D, floorTexture);
 		glActiveTexture(GL_TEXTURE1);
-		glBindTexture(GL_TEXTURE_2D, texture2);
+		glBindTexture(GL_TEXTURE_2D, wallTexture);
 
-		ourShader.use();
-
+		basicShader.use();
 		// it is best practice to call the set the projection matrix 
 		// only once outside the loop, since it rarely changes
 		glm::mat4 projection = glm::perspective(glm::radians(camera.Zoom), (float)SCREEN_WIDTH / SCREEN_HEIGHT, 0.1f, 100.0f);
-		ourShader.setMat4("projection", projection);
+		basicShader.setMat4("projection", projection);
 
 		glm::mat4 view = camera.GetViewMatrix();
-		ourShader.setMat4("view", view);
+		basicShader.setMat4("view", view);
 
-		glBindVertexArray(VAO);
-		/*for (unsigned int i = 0; i < 4; i++)
+
+		glBindVertexArray(VAOs[0]);
+		basicShader.setInt("ourTexture", 0);
+		glm::mat4 model = glm::mat4(1.0f);
+		model = glm::translate(model, floorPosition);
+		basicShader.setMat4("model", model);
+		glDrawArrays(GL_TRIANGLES, 0, 6);
+
+		glBindVertexArray(VAOs[1]);
+		basicShader.setInt("ourTexture", 1);
+		for (unsigned int i = 0; i < 4; i++)
 		{
 			glm::mat4 model = glm::mat4(1.0f);
 			model = glm::translate(model, wallPositions[i]);
-			float angle = 20.0f * i;
-			model = glm::rotate(model, glm::radians(wallRotations[0].angle), wallRotations[0].rotationVector);
-			ourShader.setMat4("model", model);
-
-			glDrawArrays(GL_TRIANGLES, 0, 36);	
-		}*/
-		glm::mat4 model = glm::mat4(1.0f);
-		ourShader.setMat4("model", model);
-		glDrawArrays(GL_TRIANGLES, 0, 6);
+			model = glm::rotate(model, glm::radians(wallRotations[i].angle), wallRotations[i].rotationVector);
+			basicShader.setMat4("model", model);
+			glDrawArrays(GL_TRIANGLES, 0, 6);
+		}
 
 		unsigned int error = glGetError();
 		while (error)
@@ -245,9 +226,9 @@ int main()
 		glfwPollEvents();
 	}
 
-	glDeleteVertexArrays(1, &VAO);
-	glDeleteBuffers(1, &VBO);
-	glDeleteProgram(ourShader.ID);
+	glDeleteVertexArrays(2, VAOs);
+	glDeleteBuffers(2, VBOs);
+	glDeleteProgram(basicShader.ID);
 
 	glfwTerminate();
 	return 0;
